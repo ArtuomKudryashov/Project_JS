38524bd8f5a16cd0879003cdf07a372c9053497c
“Artuom <artuomkudryashov@gmail.com>
2023-09-26 00:08:09 -0400

    Boolean, If Else

--- CW/Lesson_2/app.js
+++ CW/Lesson_2/app.js
@@ -5,3 +5,28 @@ if(personeAge>=21){
 }else {
     console.log("No booze for you");
 }
+
+const z = 56 > 78;
+
+const z2 = 56 !==0;
+console.log(z,z2);
+
+const z3 = 10 !=-23;
+console.log(z3)
+
+const passengerAge = 8;
+const conditional = passengerAge > 2;
+const conditional2 = passengerAge < 13;
+
+
+const z4 = 45 > 12 && 16 ===16
+const z5 = 46 !== 46 || 18>=17;
+
+console.log(z4, z5);
+
+const z6 = false&& 55 > 12
+console.log("z6 ",z6);
+
+const z7 = true || false;
+console.log ("z7",z7);
+ 
